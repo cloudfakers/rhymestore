ba8bdee545dbd06a8fb4c48236c98cbde92a7e2f
Ignasi Barrera <ignasi.barrera@gmail.com>
2010-10-25 18:23:52 +0200

    Revert "Added LGLP license"
    This reverts commit 97e6ae222c1bd4fe329f436e896f00248ebce3c6.

--- src/main/java/com/rhymestore/store/Keymaker.java
+++ src/main/java/com/rhymestore/store/Keymaker.java
@@ -1,22 +1,3 @@
-/**
- * The Rhymestore project.
- *
- * This application is free software; you can redistribute it and/or
- * modify it under the terms of the GNU LESSER GENERAL PUBLIC
- * LICENSE as published by the Free Software Foundation under
- * version 3 of the License
- *
- * This application is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * LESSER GENERAL PUBLIC LICENSE v.3 for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the
- * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
- * Boston, MA 02111-1307, USA.
- */
-
 package com.rhymestore.store;
 
 import java.lang.StringBuilder;
