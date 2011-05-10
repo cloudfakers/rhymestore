55fd1baa84345627a321b18b541a9a2e0480bfe2
Ignasi Barrera <ignasi.barrera@abiquo.com>
2011-05-10 15:09:20 +0200

    Formatting

--- src/test/java/com/rhymestore/lang/es/SpanishNumberTest.java
+++ src/test/java/com/rhymestore/lang/es/SpanishNumberTest.java
@@ -19,10 +19,6 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-
-/**
- * 
- */
 package com.rhymestore.lang.es;
 
 import static com.rhymestore.lang.es.SpanishNumber.getBaseSound;
